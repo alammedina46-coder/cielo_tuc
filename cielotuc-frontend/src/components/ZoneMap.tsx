4d4e4ca59d5f420c68cf49d5afd34fb109de63fa
Alexis <alammedina46@gmail.com>
2026-09-07 02:26:21 -0300

    fix: use Esri WorldStreetMap tiles (CARTO requires API key)

--- cielotuc-frontend/src/components/ZoneMap.tsx
+++ cielotuc-frontend/src/components/ZoneMap.tsx
@@ -19,8 +19,8 @@ export function ZoneMap({ zones }: Props) {
         zoomControl={true}
       >
         <TileLayer
-          attribution='&copy; <a href="https://carto.com/">CARTO</a>'
-          url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+          attribution='&copy; Esri &mdash; Esri, DeLorme, NAVTEQ'
+          url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}"
         />
         {zones.map((z) => (
           <Marker
